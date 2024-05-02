0ef9d9f5b0708d2d1c877e55154f59cee8350b83
eseoseodion <ehodion1@sheffield.ac.uk>
2024-05-02 16:10:26 +0100

    Customised the admin interface

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Form
 
-admin.site.register(Form)
+# this class inherits from the method 'admin' of the class 'ModelAdmin
+class FormAdmin(admin.ModelAdmin): 
+    list_display = ('first_name', 'last_name', 'email') # display this information for in candidate
+    search_fields = ('first_name', 'last_name', 'email') # apply a search field bar to search for candidates
+    list_filter = ('date', 'occupation') # apply a filter to the admin site
+    ordering = ('first_name', ) # display names in alphabetical order
+    readonly_fields = ('occupation', ) # let this field be read only, make it uneditable
+
+
+admin.site.register(Form, FormAdmin)
