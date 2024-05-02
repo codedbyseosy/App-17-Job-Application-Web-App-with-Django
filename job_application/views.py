fac76ac3e132fba7272841e4665505018e02d24b
eseoseodion <ehodion1@sheffield.ac.uk>
2024-05-02 17:17:58 +0100

    Implemented a navigation bar and added a 'contact' page

--- job_application/views.py
+++ job_application/views.py
@@ -30,5 +30,10 @@ def index(request):
             messages.success(request, "Form submitted successfully!")
     return render(request, "index.html")
 
+
 def about(request):
     return render(request, "about.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
