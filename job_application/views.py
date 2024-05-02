6fe3d98fdf365a6196a4c61e2b8b68b947be97d8
eseoseodion <ehodion1@sheffield.ac.uk>
2024-05-02 16:52:52 +0100

    Created a base template and an about page

--- job_application/views.py
+++ job_application/views.py
@@ -29,3 +29,6 @@ def index(request):
 
             messages.success(request, "Form submitted successfully!")
     return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
